b97e5bb070bf55724487500ce6e9af76e63ecd01
reham <reham2002ibrahim@gmail.com>
2025-05-12 23:19:55 +0300

    create HomeScreen

---
+++ Sports-App/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  Sports-App
+//
+//  Created by MAC on 12/05/2025.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
